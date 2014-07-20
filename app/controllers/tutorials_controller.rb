62c9226f696e9ecec24d895c1b911e332a488304
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2014-07-20 16:35:45 -0400

    Added Editor Form to put user input and show output

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -18,14 +18,38 @@ class TutorialsController < ApplicationController
   def sectionize_tutorial
     page = Sectionizer.new params[:url]
     sections = page.sectionize
-
-    File.open('app/views/tutorials/show.html.erb','w') do |file|
+ 
+    File.open('app/views/tutorials/show.html.erb', 'w') do |file|
+      file.write "<%= button_tag 'Show editor', id: 'editor-button' %>"
+      
       sections.each_key do |key|
-        file.write sections[key][:title]
+        #binding.pry
+        file.write(sections[key][:title])
         #file.write sections[key][:content]
         #binding.pry
       end
+      file.write('<div id="todo-editor">
+                     <div id="editor">function foo(items) {
+                        var x = "All this is syntax highlighted"
+                        return x;
+                       }</div><br>
+                       <%=form_tag "/tutorials", method: "post"  do %> 
+                          <%= label_tag :input %><br>
+                          <%= text_area_tag :input %><br>
+                          <%= label_tag :output %><br>
+                          <%= text_area_tag :output %><br>
+                          <%= submit_tag "Execute", id: "code-submit"%>
+                       <% end %>
+                  </div>
+                  <script>
+                    var editor = ace.edit("editor");
+                    editor.setOptions({
+                      autoScrollEditorIntoView: true,
+                      theme: "ace/theme/monokai",
+                      showPrintMargin: false,
+                      mode: "ace/mode/javascript"
+                    });
+                  </script>')
     end
   end
-    
 end
