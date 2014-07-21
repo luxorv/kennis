dcbc762eb81d2d4a38539f2eb40bc7f3ed05df31
Victor Polanco <vmp2408@gmail.com>
2014-07-21 05:47:35 -0400

    Finished profile page, specific user info needed though

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    @some_str = "asssss"
     if session[:username] == params[:username]
+      @user = User.find_by_username(params[:username])
       render 'main/index'
     else
       redirect_to '/'
